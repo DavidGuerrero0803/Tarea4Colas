40f5a6927aef62cff90e5bdb590d4d99f16ecf0d
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-09-09 20:27:12 -0700

    Corregí el funcionamiento del método insertarInicio en la clase ColaDoble.

--- src/ColaDoble.java
+++ src/ColaDoble.java
@@ -2,53 +2,38 @@ public class ColaDoble<T> {
     private T[] cola;
     private int inicio;
     private int fin;
-    private int tamaño;
 
     public ColaDoble() {
-        cola = (T[])(new Object[6]);
+        cola = (T[]) (new Object[6]);
         this.inicio = -1;
         this.fin = -1;
-        this.tamaño = 0;
     }
 
     public ColaDoble(int capacidad) {
-        cola = (T[])(new Object[capacidad]);
+        cola = (T[]) (new Object[capacidad]);
         this.inicio = -1;
         this.fin = -1;
-        this.tamaño = 0;
     }
 
     public void insertarInicio(T dato) {
-        if (tamaño == cola.length) {
-            System.out.println("Desbordamiento");
-            return;
-        }
-
-        if (tamaño == 0) {
+        if ((inicio == 0 && fin == cola.length - 1) || (inicio == 0 && fin != -1)) {
+            if (fin < cola.length - 1) {
+                for (int i = fin; i >= inicio; i--) {
+                    cola[i + 1] = cola[i];
+                }
+                fin++;
+            } else {
+                System.out.println("Desbordamiento");
+                return;
+            }
+        } else if (inicio == -1) {
             inicio = 0;
             fin = 0;
         } else {
-            inicio = (inicio - 1 + cola.length) % cola.length;
+            inicio--;
         }
 
         cola[inicio] = dato;
-        tamaño++;
     }
 
-    public void insertarFin(T dato) {
-        if (tamaño == cola.length) {
-            System.out.println("Desbordamiento");
-            return;
-        }
-
-        if (tamaño == 0) {
-            inicio = 0;
-            fin = 0;
-        } else {
-            fin = (fin + 1) % cola.length;
-        }
-
-        cola[fin] = dato;
-        tamaño++;
-    }
 }
